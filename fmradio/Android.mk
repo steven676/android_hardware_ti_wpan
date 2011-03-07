d7836ff7b9f231ef097bddfaefe72ee164f075f0
Jean Johnson <jean-johnson@ti.com>
2011-03-07 14:09:41 -0800

    Revert "OMAP3: Update the FM stack to newer version"
    This reverts commit 198f05b85d5a6b3a6320aa6d3bc5bec921f8f920.
    
    Reverting as it breaks the Android File System build.
    
    Change-Id: Ibc3074e937ac5095e8343bf389f0d7744c34fca5
    Signed-off-by: Jean Johnson <jean-johnson@ti.com>

--- fmradio/Android.mk
+++ fmradio/Android.mk
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+ifeq ($(TARGET_BOARD_PLATFORM),omap4)
 ifeq ($(BUILD_FM_RADIO),true)
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
 ifeq ($(FM_CHR_DEV_ST),true)
@@ -20,5 +21,6 @@ include hardware/ti/wpan/fmradio/fm_chrlib/Android.mk
 endif
 include hardware/ti/wpan/fmradio/fm_stack/Android.mk
 include hardware/ti/wpan/fmradio/fm_app/Android.mk
+endif
 endif
 endif
